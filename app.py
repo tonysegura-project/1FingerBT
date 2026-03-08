321497eb8f181a91a2490117990a93b26861040e
Antonio Segura <tonysegura150202@gmail.com>
2026-03-08 16:14:13 -0400

    added database support

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask, render_template, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
 
 data = {}
-
+class Behavior(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    count = db.Column(db.Integer, default=0)
 @app.route("/")
 def index():
     return render_template("index.html", data=data)
@@ -22,7 +30,11 @@ def update():
         data[behavior] += 1
     return jsonify(data)
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == "__main__":
     app.run()
+
     #python app.py
     
