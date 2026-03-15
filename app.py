50191c3651c47a860ff4abfe08ab15f68ae3a9e4
Antonio Segura <tonysegura150202@gmail.com>
2026-03-15 15:21:44 -0400

    Fixed database syncing, login, and mobile UI

--- app.py
+++ app.py
@@ -139,4 +139,7 @@ def delete_session(session_id):
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    # Render provides a PORT environment variable, so we use 5000 as a backup
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
